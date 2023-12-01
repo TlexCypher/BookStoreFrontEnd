8d62e2134b14533e2dee6fa911ee30e9c37d9fbd
Taichi Araki <araki@fun.bio.keio.ac.jp>
2023-12-01 17:00:34 +0900

    Successfuly implement all apis.

--- src/pages/DeleteBooks.jsx
+++ src/pages/DeleteBooks.jsx
@@ -1,10 +1,66 @@
-// import React from 'react'
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { useParams, useNavigate } from 'react-router-dom';
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const DeleteBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(false);
+    axios
+    .get(`http://localhost:8000/books/${id}`)
+    .then((res) => {
+      setTitle(res.data.title);
+      setAuthor(res.data.author);
+      setPublishYear(res.data.publishYear);
+      setLoading(false);
+    }).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }, []);
+
+  const handleDeleteBook = () => {
+    const data = {
+      title: title, 
+      author: author, 
+      publishYear: publishYear
+    };
+    axios
+    .delete(`http://localhost:8000/books/${id}`, data)
+    .then(() => {
+      setLoading(false);
+      navigate("/")
+    }).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  };
+
   return (
-    <div>
-      
-    </div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+        <h3 className='text-2xl'>Are You Sure You want to delete this book?</h3>
+
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteBook}
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div> 
   )
 }
 
--- src/pages/EditBooks.jsx
+++ src/pages/EditBooks.jsx
@@ -1,10 +1,94 @@
-// import React from 'react'
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
 const EditBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+    .get(`http://localhost:8000/books/${id}`)
+    .then((res) => {
+      setTitle(res.data.title);
+      setAuthor(res.data.author);
+      setPublishYear(res.data.publishYear);
+      setLoading(false);
+    }
+    ).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }, []);
+
+  const handleEditBook = () => {
+    setLoading(true);
+    const data = {
+      title: title, 
+      author: author, 
+      publishYear: publishYear
+    };
+    axios
+    .put(`http://localhost:8000/books/${id}`, data)
+    .then(() => {
+      setLoading(false);
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }
+
   return (
-    <div>
-      
-    </div>
+    <div className="p-4">
+      <BackButton/>
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+        {loading ? (
+          <Spinner/>
+        ) : (
+          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+            <div className="my-4">
+              <label className="text-xl mr-4 text-gray-500">Title</label>
+              <input
+              type='text'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+              />
+            </div>
+            <div className="my-4">
+              <label className="text-xl mr-4 text-gray-500">Author</label>
+              <input
+              type='text'
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+              />
+            </div>
+            <div className="my-4">
+              <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+              <input
+              type='text'
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+              />
+            </div>
+            <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+              Edit Save
+            </button>
+          </div> 
+        )}
+      </div>
   )
 }
 
